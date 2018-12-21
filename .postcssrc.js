5616d866abfc27565e8db959e776c3052a6bed21
likezhen <kezhen.li@ndmicro.com>
2018-12-21 11:48:49 +0800

    fix(px2rem)：修复转换插件中的bug

--- .postcssrc.js
+++ .postcssrc.js
@@ -5,8 +5,8 @@ module.exports = {
             rootValue: 100, // 用于计算的根字体大小
             minPixelValue: 1, // 小于或等于`1px`不转换为视窗单位，你也可以设置为你想要的值
             convertUnit: 'rem', // 转换成的单位
-            selectorBlackList: ['.ignore', '.hairlines'], // 忽略的选择器
-            propList: [], // 忽略的属性
+            selectorBlackList: ['.ignore', '.hairlines'], // 忽略的选择器,可以为正则表达式
+            propBlackList: [], // 忽略的属性
             mediaQuery: false, // // 是否允许在媒体查询中条件中转换`px`
         })
     ]
--- src/assets/js/px2rem.js
+++ src/assets/js/px2rem.js
@@ -1,6 +1,6 @@
 'use strict'
 
-var postcss = require('postcss')
+const postcss = require('postcss')
 
 // excluding regex trick: http://www.rexegg.com/regex-best-trick.html
 // 双引号中没有内容
@@ -8,37 +8,36 @@ var postcss = require('postcss')
 // url()中没有内容不够
 // 后面有px的数字
 // !singlequotes|!doublequotes|!url()|pixelunit
-var pxRegex = /"[^"]+"|'[^']+'|url\([^\u0029]+\)|(\d*\.?\d+)px/ig
+const pxRegex = /"[^"]+"|'[^']+'|url\([^\u0029]+\)|(\d*\.?\d+)px/ig
 
-var defaults = {
+const defaults = {
     rootValue: 100, // 用于计算的根字体大小
     minPixelValue: 1, // 小于或等于`1px`不转换为视窗单位，你也可以设置为你想要的值
     convertUnit: 'rem', // 转换成的单位
     selectorBlackList: ['.ignore'], // 忽略的选择器
-    propList: [], // 忽略的属性
+    propBlackList: [], // 忽略的属性
     mediaQuery: false, // // 是否允许在媒体查询中条件中转换`px`
 }
 
 module.exports = postcss.plugin('postcss-px-to-rem', function (options) {
 
-    var opts = Object.assign({}, defaults, options)
-    var pxReplace = createPxReplace(opts.rootValue, opts.minPixelValue, opts.convertUnit)
+    const opts = Object.assign({}, defaults, options)
+    const pxReplace = createPxReplace(opts.rootValue, opts.minPixelValue, opts.convertUnit)
 
-    return function (css) {
-
-        css.walkDecls(function (decl, i) {
+    return function (root) {
+        root.walkDecls(function (decl, i) {
             // This should be the fastest test and will remove most declarations
             if (decl.value.indexOf('px') === -1) return
 
-            if (blacklistedSelector(opts.selectorBlackList, decl.prop)) return
+            if (blacklisted(opts.propBlackList, decl.prop)) return
 
-            if (blacklistedSelector(opts.selectorBlackList, decl.parent.selector)) return
+            if (blacklisted(opts.selectorBlackList, decl.parent.selector)) return
 
             decl.value = decl.value.replace(pxRegex, pxReplace)
         })
 
         if (opts.mediaQuery) {
-            css.walkAtRules('media', function (rule) {
+            root.walkAtRules('media', function (rule) {
                 if (rule.params.indexOf('px') === -1) return
                 rule.params = rule.params.replace(pxRegex, pxReplace)
             })
@@ -61,10 +60,10 @@ function createPxReplace (rootValue, minPixelValue, convertUnit) {
     }
 }
 
-function blacklistedSelector (blacklist, selector) {
-    if (typeof selector !== 'string') return
+function blacklisted (blacklist, name) {
+    if (typeof name !== 'string') return
     return blacklist.some(function (regex) {
-        if (typeof regex === 'string') return selector.indexOf(regex) !== -1
-        return selector.match(regex)
+        if (typeof regex === 'string') return name.indexOf(regex) !== -1
+        return name.match(regex)
     })
 }
